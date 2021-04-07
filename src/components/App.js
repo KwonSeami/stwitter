9b58955009311847e88c539359c6a4ba9f0901a5
권샘이 <kwonsam07@daum.net>
2021-04-08 02:16:44 +0900

    :sparkles: firebase 이름 수정, authService 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import {authService} from "firebaseJS";
 
 function App() {
-  const [isLoggedIn, setIsLiggedIn] = useState(false);
 
-  return <AppRouter isLoggedIn={isLoggedIn}/>;
+  const [isLoggedIn, setIsLiggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; Stwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
